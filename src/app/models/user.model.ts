e49ec62634b604481bbfd98e844f14e93c06ae6b
legacy0013 <remy.galopin@gmail.com>
2019-06-12 10:24:01 +0200

    user.component

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,10 @@
+export class User {
+
+  userId: string;
+  userFirstname: string;
+  userLastname: string;
+  userEmail: string;
+  userPhone: string;
+  userRole: number;
+  userStatus: string;
+}
